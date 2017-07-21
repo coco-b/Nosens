9ba6f053627c46fd1bfd97f72c573f16fab16cd6
Cecile_Berger <ccl.berger@gmail.com>
2017-07-21 18:06:56 +0200

    form install

--- src/Nosens/WcsBundle/Controller/WcsController.php
+++ src/Nosens/WcsBundle/Controller/WcsController.php
@@ -4,7 +4,10 @@ namespace Nosens\WcsBundle\Controller;
 
 
 
+use Nosens\WcsBundle\Entity\Card;
+use Nosens\WcsBundle\Form\CardType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class WcsController extends Controller
@@ -12,6 +15,30 @@ class WcsController extends Controller
     public function HomeAction()
     {
         return $this->render('@NosensWcs/Homepage.html.twig');
+    }
+
+    public function WcsaddAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $card = new Card();
+        $form = $this->createForm(CardType::class, $card);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($card);
+            $em->flush();
+
+            return $this->redirectToRoute('wcs_card_add');
+        }
+
+        return $this->render('@NosensWcs/Wcs.html.twig', array(
+                'card' => $card,
+                'form' => $form->createView(),
+            ));
+
+
     }
 }
 
---
+++ src/Nosens/WcsBundle/Form/CardType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Nosens\WcsBundle\Form;
+
+
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CardType extends AbstractType
+
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+       $builder
+           ->add('title', TextareaType::class)
+           ->add('content',TextareaType::class)
+           ->add('date',DateType::class)
+           ->add('link',TextareaType::class)
+           ->add('submit',SubmitType::class, array('label'=>'Valider'));
+    }
+
+
+        /**
+         * {@inheritdoc}
+         */
+        public function configureOptions(OptionsResolver $resolver)
+        {
+            $resolver->setDefaults(array(
+                'data_class' => 'Nosens\WcsBundle\Entity\Card'
+            ));
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function getBlockPrefix()
+        {
+            return 'wcsbundle_card';
+        }
+
+}
